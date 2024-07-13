c4e667c31eeaf04a9131263776aa92881627a5c3
Documenter.jl <documenter@juliadocs.github.io>
2024-07-13 21:19:27 +0000

    build based on a2f9868

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = RadialSchrodinger","category":"page"},{"location":"#RadialSchrodinger","page":"Home","title":"RadialSchrodinger","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for RadialSchrodinger.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [RadialSchrodinger]","category":"page"}]
+}
